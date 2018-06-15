1f69a9fec3e8da5d25cb350682ba3c5fe3cd7933
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-06-15 04:46:58 -0700

    Defining "include_guard" macro if CMake < 3.10.0

--- cmake/Modules/MacroUtilities.cmake
+++ cmake/Modules/MacroUtilities.cmake
@@ -874,6 +874,65 @@ endmacro(CHECKOUT_CMAKE_SUBMODULE)
 
 
 #------------------------------------------------------------------------------#
+# only define if less than 3.10.0
+#
+if(CMAKE_VERSION VERSION_LESS "3.10.0")
+macro(include_guard)
+
+    if("${ARGN}" STREQUAL "GLOBAL")
+        set(_GLOBAL ON)
+    elseif("${ARGN}" STREQUAL "DIRECTORY")
+        set(_DIRECTORY ON)
+    else()
+        set(_VARIABLE ON)
+    endif()
+
+    set(_VARNAME __${CMAKE_CURRENT_LIST_FILE}_VAR__)
+
+    if(_GLOBAL)
+        get_property(_PROP
+            GLOBAL
+            PROPERTY ${_VARNAME_}
+            SET
+        )
+        if(_PROP)
+            return()
+        endif()
+        set_property(GLOBAL PROPERTY ${_VARNAME_} TRUE)
+
+    elseif(_DIRECTORY)
+        get_property(_PROP
+            DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            PROPERTY ${_VARNAME_}
+            SET
+        )
+
+        if(_PROP)
+            return()
+        endif()
+
+        set_property(DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+            PROPERTY ${_VARNAME_} TRUE)
+
+    elseif(_VARIABLE)
+        get_property(_PROP
+            VARIABLE
+            PROPERTY ${_VARNAME_}
+            SET
+        )
+
+        if(_PROP)
+            return()
+        endif()
+
+        set_property(PROPERTY ${_VARNAME_} TRUE)
+    endif()
+
+endmacro(include_guard)
+endif(CMAKE_VERSION VERSION_LESS "3.10.0")
+
+
+#------------------------------------------------------------------------------#
 # always determine the default lib directory
 DETERMINE_LIBDIR_DEFAULT(LIBDIR_DEFAULT)
 
