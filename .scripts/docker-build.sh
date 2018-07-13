11a466a73639eaa5f781dd947a4a45e3b84c9cd1
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-07-13 12:29:58 -0700

    Fix to docker-build.sh script

--- .scripts/docker-build.sh
+++ .scripts/docker-build.sh
@@ -6,8 +6,10 @@ if [ ${PWD} = ${BASH_SOURCE[0]} ]; then
     cd ..
 fi
 
-LATEST="3.6"
-VERSIONS="2.7 3.4 3.5 3.6 3.7"
+# set if not set
+: ${LATEST:="3.6"}
+: ${VERSIONS:="2.7 3.4 3.5 3.6 3.7"}
+
 BUILD_DIR=${PWD}
 
 for i in ${VERSIONS}
@@ -32,8 +34,9 @@ while [ ! -z "$(ps | grep 'docker push' | grep -v ' grep ')" ];
 do
     echo -e "Waiting for docker push to complete..."
     ps | grep 'docker push' | grep -v ' grep '
-    sleep 30
+    sleep 10
 done
 
-docker tag jrmadsen/pyctest:py${LATEST} jrmadsen/pyctest:latest
+LATEST_TAG=$(echo ${LATEST} | sed 's/\.//g')
+docker tag jrmadsen/pyctest:py${LATEST_TAG} jrmadsen/pyctest:latest
 docker push jrmadsen/pyctest:latest
