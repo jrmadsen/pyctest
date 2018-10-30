da50427be92a291093a96bbeb8e9d86e313585c0
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-10-30 10:30:56 -0700

    Helpers update -- GetSystemVersionInfo function

--- .scripts/conda-build.sh
+++ .scripts/conda-build.sh
@@ -14,6 +14,7 @@ if [ -f "${PWD}/meta.yaml" ]; then
     for i in 3.6 3.5 2.7 3.4 3.7
     do
         conda-build -c conda-forge -c jrmadsen --override-channels --python=${i} .
+        conda-build purge
     done
     conda config --set anaconda_upload no
 fi
