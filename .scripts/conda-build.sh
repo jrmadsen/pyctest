fe6f0604a5d6940d857a6bd30c5599fd8f771695
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-12-06 22:08:12 -0800

    Script update + remove build.sh

--- .scripts/conda-build.sh
+++ .scripts/conda-build.sh
@@ -6,14 +6,36 @@ if [ ${PWD} = ${BASH_SOURCE[0]} ]; then
     cd ..
 fi
 
-module load python/conda
+command-exists ()
+{
+    command -v $@
+}
+
+if [ ! -z "$(command-exists module)" ]; then
+    if [ -n "$(module avail python &> /dev/stdout | grep 'python/conda')" ]; then
+        module load python/conda
+    fi
+fi
+
+if [ -z "$(command-exists conda)" ]; then
+    echo "Unable to find conda executiable"
+    exit 1
+fi
+
+if [ -z "$(command-exists conda-build)" ]; then
+    echo "Unable to find conda-build script"
+    exit 1
+fi
 
 if [ -f "${PWD}/meta.yaml" ]; then
     ./update-conda-yaml.py
     conda config --set anaconda_upload yes
-    for i in 3.6 3.5 2.7 3.4 3.7
+    for i in 2.7 3.{6,7,4,5}
     do
-        conda-build -c conda-forge -c jrmadsen --override-channels --python=${i} .
+        conda-build \
+            --cache-dir=/tmp/pyctest-conda-build \
+            -c conda-forge -c jrmadsen \
+            --override-channels --python=${i} .
         conda-build purge
     done
     conda config --set anaconda_upload no
